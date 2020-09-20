1a6122fafbad5bfca08614924eb3db281259404f
JeffreyFong <leanwf1117@gmail.com>
2020-09-21 02:22:02 +0800

    chatla backend

---
+++ server.js
@@ -0,0 +1,76 @@
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+  appId: "1075864",
+  key: "ddc4f9fc71b16a4c66ba",
+  secret: "089784769f2d7ffeb571",
+  cluster: "ap1",
+  encrypted: true,
+});
+
+app.use(express.json());
+app.use(cors());
+
+const connectionURL =
+  "mongodb+srv://admin:g289QXEp6KFGEEnM@cluster0.qh3tb.mongodb.net/<chatladb>?retryWrites=true&w=majority";
+mongoose.connect(connectionURL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("db connected");
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log(change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("error pusher");
+    }
+  });
+});
+
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.post("/message/new", (req, res) => {
+  const dbMessage = req.body;
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/message/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.listen(port, () => console.log(`listening on localhost:${port}`));
